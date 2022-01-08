5fdbeb6f7c8fe7bcedae14f708ed9de0bee8acec
rafa <rafa>
2022-01-08 05:18:20 -0300

    done?

--- sorting.c
+++ sorting.c
@@ -146,45 +146,41 @@ int	find_next_position(s_node *stack_b)
 void	sort_with_n(s_node *stack_a, s_node *stack_b, int len)
 {
 	//divide in n chunks until len of stack_a equal 0
-	int counter = 0;
-	while(stack_length(stack_a) > 0)
-	{
-		while(counter < 40)
+	int chunk = 30;
+    int up = (len / 2) + chunk;
+    int down = (len / 2) - chunk;
+
+    int last_up = len / 2;
+    int last_down = len / 2;
+
+    int counter = 0;
+    while(stack_length(stack_a) > 0)
+    {
+        if (FIRST_NODE_A->index >= last_up && FIRST_NODE_A->index < up)
 		{
-			if (FIRST_NODE_A->index >= 40 && FIRST_NODE_A->index < 60)
-			{
-				counter++;
-				pb(stack_a, stack_b);
-			}
-			else if (FIRST_NODE_A->index >= 20 && FIRST_NODE_A->index < 40)
-			{
-				counter++;
-				pb(stack_a, stack_b);
-				if(stack_length(stack_b) > 1)
-					rb(stack_b);
-			}
-			else
-				ra(stack_a);
-		}
-		while(counter < 80)
-		{
-			if (FIRST_NODE_A->index >= 60 && FIRST_NODE_A->index < 80)
-			{
-				counter++;
-				pb(stack_a, stack_b);
-			}
-			else if (FIRST_NODE_A->index >= 0 && FIRST_NODE_A->index < 20)
-			{
-				counter++;
-				pb(stack_a, stack_b);
-				rb(stack_b);
-			}
-			else
-				ra(stack_a);
-		}
-		while(stack_length(stack_a) > 0)
+			counter++;
 			pb(stack_a, stack_b);
-	}
+		}
+        else if (FIRST_NODE_A->index >= down && FIRST_NODE_A->index < last_down)
+        {
+            counter++;
+            pb(stack_a, stack_b);
+            if(stack_length(stack_b) > 1)
+                rb(stack_b);
+        }
+        else
+		{
+            ra(stack_a);
+		}
+        if (counter == chunk * 2)
+        {
+            last_up = up;
+            last_down = down;
+            down -= chunk;
+            up += chunk;
+			counter = 0;
+        }
+    }
 	//bring back numbers in order
 	while(stack_length(stack_b) != 0)
 	{
