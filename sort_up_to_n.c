cf3f4812ee11a5169e8963099379d6343d830bcf
rafa <rafa>
2022-01-09 23:59:58 -0300

    finishing sort n norm

--- sort_up_to_n.c
+++ sort_up_to_n.c
@@ -5,12 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpaulino <rpaulino@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/09 21:54:54 by rpaulino          #+#    #+#             */
-/*   Updated: 2022/01/09 21:55:10 by rpaulino         ###   ########.fr       */
+/*   Created: 2022/01/09 23:41:35 by rpaulino          #+#    #+#             */
+/*   Updated: 2022/01/09 23:59:21 by rpaulino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+typedef struct sort
+{
+	int	up;
+	int	down;
+	int	last_up;
+	int	last_down;
+	int	len;
+}	t_sort;
 
 int	find_next_position(t_node *stack_b)
 {
@@ -39,52 +48,12 @@ int	find_next_position(t_node *stack_b)
 	return (max_position);
 }
 
-void	sort_with_n(t_node *stack_a, t_node *stack_b, int len)
+void	bring_back_to_stack_a(t_node *stack_a, t_node *stack_b)
 {
 	int	len_b;
 	int	position;
 	int	swap_signal;
-	int	chunk;
-	int	up;
-	int	down;
-	int	last_up;
-	int	last_down;
-	int	counter;
 
-	chunk = 14;
-	if (len > 450)
-		chunk = 29;
-	up = (len / 2) + chunk;
-	down = (len / 2) - chunk;
-	last_up = len / 2;
-	last_down = len / 2;
-	counter = 0;
-	while (stack_length(stack_a) > 0)
-	{
-		if (stack_a->next->index >= last_up && stack_a->next->index < up)
-		{
-			counter++;
-			pb(stack_a, stack_b);
-		}
-		else if (stack_a->next->index >= down
-			&& stack_a->next->index < last_down)
-		{
-			counter++;
-			pb(stack_a, stack_b);
-			if (stack_length(stack_b) > 1)
-				rb(stack_b);
-		}
-		else
-			ra(stack_a);
-		if (counter == chunk * 2)
-		{
-			last_up = up;
-			last_down = down;
-			down -= chunk;
-			up += chunk;
-			counter = 0;
-		}
-	}
 	while (stack_length(stack_b) != 0)
 	{
 		len_b = stack_length(stack_b);
@@ -120,4 +89,55 @@ void	sort_with_n(t_node *stack_a, t_node *stack_b, int len)
 		if (swap_signal)
 			sa(stack_a);
 	}
+}
+
+int	send_to_stack_b(t_node *stack_a, t_node *stack_b, int counter, int mode)
+{
+	pb(stack_a, stack_b);
+	if (stack_length(stack_b) > 1 && mode)
+		rb(stack_b);
+	return (counter + 1);
+}
+
+void	send_chunks_to_stack_b(t_node *stack_a, t_node *stack_b,
+								t_sort *data, int chunk)
+{
+	int	counter;
+
+	counter = 0;
+	while (stack_length(stack_a) > 0)
+	{
+		if (stack_a->next->index >= data->last_up
+			&& stack_a->next->index < data->up)
+			counter = send_to_stack_b(stack_a, stack_b, counter, 0);
+		else if (stack_a->next->index >= data->down
+			&& stack_a->next->index < data->last_down)
+			counter = send_to_stack_b(stack_a, stack_b, counter, 1);
+		else
+			ra(stack_a);
+		if (counter == chunk * 2)
+		{
+			data->last_up = data->up;
+			data->last_down = data->down;
+			data->down -= chunk;
+			data->up += chunk;
+			counter = 0;
+		}
+	}
+}
+
+void	sort_with_n(t_node *stack_a, t_node *stack_b, int len)
+{
+	int		chunk;
+	t_sort	data;
+
+	chunk = 14;
+	if (len > 450)
+		chunk = 29;
+	data.up = (len / 2) + chunk;
+	data.down = (len / 2) - chunk;
+	data.last_up = len / 2;
+	data.last_down = len / 2;
+	send_chunks_to_stack_b(stack_a, stack_b, &data, chunk);
+	bring_back_to_stack_a(stack_a, stack_b);
 }
